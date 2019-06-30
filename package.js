9c4b133b3ed9885d86e9886242645bdd78a62c87
pranav-2 <52380347+pranav-2@users.noreply.github.com>
2019-06-30 23:56:10 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+
+{
+    "name": "Vikya-cha-BHAU",
+    "version": "1.0.0",
+    "description": "This is a simple test and experiment bot",
+    "main": "Vikya-cha-bhau.js",
+    "author": "Marathi彡Pranav",
+    "scripts": {
+        "start": "node bot.js"
+    },
+    "dependencies": {
+	"discord.js" : "11.2.1"
+	}
+}
