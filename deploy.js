62f33752e505611187c4295bd952313fcec86e9e
pranav-2 <52380347+pranav-2@users.noreply.github.com>
2019-07-01 01:54:48 +0530

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,21 @@
+$ git push heroku master
+Counting objects: 3, done.
+Delta compression using up to 4 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 531 bytes | 0 bytes/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+remote: Compressing source files... done.
+remote: Building source:
+remote: 
+remote:  !     No default language could be detected for this app.
+remote:             HINT: This occurs when Heroku cannot detect the         buildpack to use for this application automatically.
+remote:             See https://devcenter.heroku.com/articles/buildpacks
+remote: 
+remote:  !     Push failed
+remote: Verifying deploy....
+remote: 
+remote: !   Push rejected to salty-coast-14102.
+remote: 
+To https://git.heroku.com/salty-coast-14102.git
+ ! [remote rejected] master -> master (pre-receive hook declined)
+error: failed to push some refs to 'https://git.heroku.com/salty-coast-14102.git'
